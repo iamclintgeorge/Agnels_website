6f6a6ba5ef272a2542c5fc30c57a0774b10d16a4
UnagiCodezz <mustansirbhagat@gmail.com>
2024-08-25 23:32:37 +0530

    Added/Finished Welcome Section and Announcements Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/Header.jsx";
 import News from "./components/News.jsx";
+import Welcome from "./components/Welcome.jsx";
 
 function App() {
   return (
     <>
       <Header />
       <News />
+      <Welcome />
     </>
   );
 }
