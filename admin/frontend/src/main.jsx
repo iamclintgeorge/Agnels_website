e6bd2383a589a7a15b6aa737e12f8016bc1ad738
Clint George <clintbgeorge@gmail.com>
2025-01-07 21:29:58 +0530

    Added authMiddleware
    Signed-off-by: Clint George <clintbgeorge@gmail.com>

--- admin/frontend/src/main.jsx
+++ admin/frontend/src/main.jsx
@@ -2,14 +2,11 @@ import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-// Import Pages
 import HomePage from './pages/homePage';
 import Login from './pages/login';
 import Signup from './pages/signup';
 import Error404 from './pages/error404';
 
-// Main App Component
 const App = () => {
   return (
     <Router>
@@ -23,7 +20,6 @@ const App = () => {
   );
 };
 
-// Render App to DOM
 const rootElement = document.getElementById('root');
 if (rootElement) {
   createRoot(rootElement).render(
