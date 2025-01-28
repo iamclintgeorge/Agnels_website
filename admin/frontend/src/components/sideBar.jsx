7a7e54b9b79d81897489d92c87d2dfe045461097
Clint George <clintbgeorge@gmail.com>
2025-01-28 19:43:37 +0530

    Minor Changes
    Signed-off-by: Clint George <clintbgeorge@gmail.com>

--- admin/frontend/src/components/sideBar.jsx
+++ admin/frontend/src/components/sideBar.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const SideBar = () => {
   return (
     <>
-    <div className="bg-[#0C2340] mt-16 min-h-screen max-h-auto w-64 text-white pb-10 sticky top-0 -z-10">
-      <div className="font-inter text-sm font-light pl-8 pt-9 flex flex-col space-y-9">
-      <p>Dashboard</p>
-      <p>Home Page</p>
-      <p>Logs</p>
-      <p>Departments Page</p>
-      <p>Admission Page</p>
-      <p>Academics Page</p>
-      <p>Students Corner</p>
+      <div className="bg-[#0C2340] mt-16 min-h-screen max-h-auto w-64 text-white pb-10 sticky top-0 -z-10">
+        <div className="font-inter text-base font-light pl-8 pt-9 flex flex-col space-y-9">
+          <p>Dashboard</p>
+          <p>Home Page</p>
+          <p>Logs</p>
+          <p>Departments Page</p>
+          <p>Admission Page</p>
+          <p>Academics Page</p>
+          <p>Students Corner</p>
+        </div>
       </div>
-    </div>
     </>
-  )
-}
+  );
+};
 
-export default SideBar
+export default SideBar;
