7dba5aa1f3f2612319c254301e5723775aa33eab
Clint George <clintbgeorge@gmail.com>
2025-02-08 21:58:22 +0530

    Minor changes: App.jsx
    Signed-off-by: Clint George <clintbgeorge@gmail.com>

--- website/frontend/src/App.jsx
+++ website/frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import Header from "./components/Header";
 import AboutUs from "./components/AboutUs";
 import WhatsNew from "./components/WhatsNew";
 import Footer from "./components/Footer";
 import Departments from "./pages/Departments/departments";
+import Aboutus from "./pages/AboutUs/aboutus";
+import Research_Publications from "./pages/Research_Publications/Research_Publications";
+import Admission from "./pages/Admission/Admission.jsx";
 import Admissions from "./pages/admissions/admissions";
 import Computer from "./pages/Departments/branches/computer/computer";
 import Electrical from "./pages/Departments/branches/electrical/electrical";
@@ -15,7 +17,6 @@ import InformationTech from "./pages/Departments/branches/IT/informationTech";
 import Mechanical from "./pages/Departments/branches/mechanical/mechanical";
 import StudentCorner from "./pages/StudentCorner/studentCorner";
 import Login from "./pages/Login/Login";
-import TrainingPlacement from "./pages/TrainingPlacement/TrainingPlacement";
 import TrainingPlacement from "./pages/TrainingPlacement/TrainingPlacement";
 
 function App() {
@@ -36,6 +37,11 @@ function App() {
           />
           {/* Departments Page */}
           <Route path="/departments" element={<Departments />} />
+          <Route path="/aboutus" element={<Aboutus />} />
+          <Route
+            path="/research_Publications"
+            element={<Research_Publications />}
+          />
           <Route path="/admissions" element={<Admissions />} />
           <Route path="/studentCorner" element={<StudentCorner />} />
           <Route path="/computer_engineering" element={<Computer />} />
@@ -45,8 +51,6 @@ function App() {
           <Route path="/it" element={<InformationTech />} />
           <Route path="/mechanical_engineering" element={<Mechanical />} />
           <Route path="/Login" element={<Login />} />
-          <Route path="/training-placement" element={<TrainingPlacement />} />
-          <Route path="/Login" element={<Login />} /> {/* ✅ Add Login Route */}
           <Route path="/training-placement" element={<TrainingPlacement />} />
         </Routes>
       </>
