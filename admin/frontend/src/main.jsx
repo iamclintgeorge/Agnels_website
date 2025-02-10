4e3b92049bde4065515d99e5ca84ea5591d66f5d
Clint George <clintbgeorge@gmail.com>
2025-02-10 22:29:27 +0530

    Created Profile page and added routes
    Signed-off-by: Clint George <clintbgeorge@gmail.com>

--- admin/frontend/src/main.jsx
+++ admin/frontend/src/main.jsx
@@ -9,7 +9,8 @@ import Error404 from "./pages/error404";
 import AdminLayout from "./layout/adminLayout";
 import Student from "./pages/student";
 import { AuthProvider } from "./services/useAuthCheck";
-import WhatsNew from "./pages/whatsNew";
+import WhatsNew from "./pages/HomePage/whatsNew";
+import Profile from "./pages/profile";
 
 const App = () => {
   return (
@@ -19,6 +20,7 @@ const App = () => {
       <Route path="/" element={<AdminLayout />}>
         {/* <Route path="/home" element={<HomePage />} /> */}
         <Route path="/home/carousel" element={<HomePage />} />
+        <Route path="/profile" element={<Profile />} />
         <Route path="/student" element={<Student />} />
         <Route path="/whatsNew" element={<WhatsNew />} />
       </Route>
