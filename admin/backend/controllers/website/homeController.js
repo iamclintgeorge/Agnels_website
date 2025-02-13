4d0383ffdd2cde86ad1ec41269e2c8599ff0183c
Clint George <clintbgeorge@gmail.com>
2025-02-13 22:27:41 +0530

    Added /frontend/carousel : API Request is not being handled well currently
    Signed-off-by: Clint George <clintbgeorge@gmail.com>

--- admin/backend/controllers/website/homeController.js
+++ admin/backend/controllers/website/homeController.js
@@ -1,10 +1,12 @@
 export const carouselController = (req, res) => {
-  const images = [
-    { url: "/images/campus1.png", alt: "Campus View 1" },
-    { url: "/images/campus2.png", alt: "Campus View 2" },
-    { url: "/images/campus3.png", alt: "Campus View 3" },
-    { url: "/images/campus4.png", alt: "Campus View 4" },
-  ];
+  const { altText, image } = req.body;
+  console.log(altText, image);
 
-  return res.json(images);
+  // const images = [
+  //   { url: "/images/campus1.png", alt: "Campus View 1" },
+  //   { url: "/images/campus2.png", alt: "Campus View 2" },
+  //   { url: "/images/campus3.png", alt: "Campus View 3" },
+  //   { url: "/images/campus4.png", alt: "Campus View 4" },
+  // ];
+  // return res.json(images);
 };
