8fc42e888aaee63709cc421c53e483605d96f9b9
DalviAnushka <160143189+DalviAnushka@users.noreply.github.com>
2025-03-17 01:52:25 +0530

    Create IICContent.jsx

---
+++ website/frontend/src/pages/IIC/IICContent.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+
+export const Institutions_Innovation_Council = () => {
+    return (
+        <div className="p-8 bg-white min-h-screen font-sans text-gray-900">
+          <h1 className="text-4xl font-semibold mb-6">Institution's Innovation Council (IIC)</h1>
+          <div className="border-t-2 border-blue-500 my-4"></div>
+            
+            <p className="text-md text-gray-700 mb-6 leading-relaxed">
+            FCRIT has established Institution’s Innovation Council (FCRIT-IIC) as per the guidelines of ‘MoE’s Innovation Cell (MIC)’ to create a vibrant local innovation and start-up ecosystem, to facilitate the Pre-incubation and Incubation facilities for the students. FCRIT-IIC is formed to organize seminars, workshops, project validation programs, entrepreneurship boot-camps etc.
+            </p>
+            
+        </div>
+    );
+};
+
+export const Innovation_and_Startup_Policy = () => {
+    return (
+        <div className="p-8 bg-white min-h-screen font-sans text-gray-900">
+          <h1 className="text-3xl font-semibold mb-6">Innovation and Startup Policy</h1>
+          <div className="border-t-2 border-blue-500 my-4"></div>
+            
+            <p className="text-md text-gray-700 mb-6 leading-relaxed">
+            FCRIT has adopted the Innovation and Startup Policy (FCRIT-ISP) as per the guidelines of ‘MoE’s Innovation Cell (MIC)’ to create a dynamic ecosystem for the students and faculty to avail the entrepreneurial opportunities and facilities provided by the Institute also to make the entrepreneurial world accessible to them.
+            </p>
+            <iframe
+        src="/pdfs/FCRIT-ISP.pdf"
+        width="100%"
+        height="800px"
+        title="FCRIT-ISP"
+      />
+      <h1 className="text-3xl font-semibold mt-12 mb-6">Notification</h1>
+      <div className="border-t-2 border-blue-500 my-4"></div>
+      <iframe
+        src="/pdfs/Notification.pdf"
+        width="100%"
+        height="800px"
+        title="Notification"
+      />
+      <h1 className="text-3xl font-semibold mt-12 mb-6">Key Implementation Team</h1>
+          <div className="border-t-2 border-blue-500 my-4"></div>
+          <iframe
+        src="/pdfs/Key-Implementation-Team.pdf"
+        width="100%"
+        height="800px"
+        title="Key-Implementation-Team"
+      />
+        <h1 className="text-3xl font-semibold mt-12 mb-6">Action Plan ISP</h1>
+          <div className="border-t-2 border-blue-500 my-4"></div>
+          <iframe
+        src="/pdfs/Action-Plan-ISP.pdf"
+        width="100%"
+        height="800px"
+        title="Action-Plan-ISP"
+      />
+          </div>
+    );
+};
+
+export const Innovation_Ambassador = () => {
+    return (
+        <div id="gr" className="p-4 bg-white shadow rounded-lg mt-4">
+            <h2 className="text-3xl font-semibold">Innovation Ambassador</h2>
+            <div className="border-t-2 border-blue-500 my-4"></div>
+            <p className="text-lg text-gray-700 mb-6 leading-relaxed">
+            </p>
+        </div>
+    );
+};
+
+export const Centre_of_Innovation_and_Entrepreneurship = () => {
+    return (
+        <div id="gr" className="p-4 bg-white shadow rounded-lg mt-4">
+            <h2 className="text-3xl font-semibold">Centre of Innovation and Entrepreneurship</h2>
+            <div className="border-t-2 border-blue-500 my-4"></div>
+            <p className="text-lg text-gray-700 mb-6 leading-relaxed">
+            </p>
+        </div>
+    );
+};
+
+
