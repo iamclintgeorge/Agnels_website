5823812438cc756b4dea0c294ca8028da09391e2
Clint George <152721610+iamclintgeorge@users.noreply.github.com>
2025-04-15 21:19:58 +0530

    Update userRoutes.js

--- admin/backend/routes/admin/userRoutes.js
+++ admin/backend/routes/admin/userRoutes.js
@@ -1,63 +1,29 @@
-// import express from "express";
-// import {
-//   signupController,
-//   loginController,
-//   logoutController,
-// } from "../../controllers/admin/userController.js";
-// import { authMiddleware, checkRole } from "../../middlewares/authMiddleware.js";
-
-// const router = express.Router();
-
-// router.post("/signup", signupController);
-// router.post("/login", loginController);
-// router.post("/signout", logoutController);
-// router.get("/check-auth", authMiddleware);
-
-
-// router.get(
-//   "/student",
-//   authMiddleware,
-//   checkRole(["superAdmin", "teach_staff","hod"]),
-//   (req, res) => {
-//     res.status(200).json({ message: "Welcome, Teacher!" });
-//     console.log("/student router");
-//   }
-// );
-
-// export default router;
-
-
 import express from "express";
 import {
   signupController,
   loginController,
   logoutController,
 } from "../../controllers/admin/userController.js";
-import { authMiddleware, checkRole } from "../../middlewares/authMiddleware.js";
+import {
+  authMiddleware,
+  checkAuth,
+  checkRole,
+} from "../../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/signup", signupController);
 router.post("/login", loginController);
 router.post("/signout", logoutController);
+router.get("/check-auth", authMiddleware, checkAuth);
 
-router.get("/check-auth", (req, res) => {
-  console.log("Handling /api/check-auth"); // Debug log
-  if (req.session && req.session.user) {
-    return res.status(200).json({
-      authenticated: true,
-      user: req.session.user,
-    });
-  }
-  return res.status(401).json({ authenticated: false });
-});
-
+//Sample test route
 router.get(
   "/student",
   authMiddleware,
-  checkRole(["superAdmin", "teach_staff", "hod"]),
+  checkRole(["superAdmin", "teach_staff"]),
   (req, res) => {
-    res.status(200).json({ message: "Welcome, Teacher!" });
+    res.status(200).json({ content: "Welcome, Teacher!" });
     console.log("/student router");
   }
 );
