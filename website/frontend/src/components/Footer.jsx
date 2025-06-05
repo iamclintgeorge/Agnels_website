e0a34e2939b0f98a474a95b9823023f06c25df79
Clint George <clintbgeorge@gmail.com>
2025-06-05 20:35:47 +0530

    Added links to footer and removed Whitespaces in HomePage. [UI Changes]
    Signed-off-by: Clint George <clintbgeorge@gmail.com>

--- website/frontend/src/components/Footer.jsx
+++ website/frontend/src/components/Footer.jsx
@@ -14,43 +14,45 @@ const Footer = () => {
       <footer className="bg-[#EEEBE5] pt-20 px-10 py-10">
         <div className="footer-container max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-4 gap-5">
           {/* Logo & Institute Info */}
-          <div className="footer-section text-center md:text-left">
-            <div className="ml-24 mb-2">
-              <img
-                src="/src/assets/imgs/fcritlogo.png"
-                alt="FCRIT Logo"
-                className="w-20 h-20 mx-auto md:mx-0 mb-1"
-              />
-            </div>
-            <div className="pl-0 pt-0">
-              <div className="relative top-3 left-1">
-                <p className="font-inter text-[10px] font-light leading-9 tracking-[1.0px] italic">
-                  Agnel Charities
-                </p>
+          <a href="/">
+            <div className="footer-section text-center md:text-left">
+              <div className="ml-24 mb-2">
+                <img
+                  src="/src/assets/imgs/fcritlogo.png"
+                  alt="FCRIT Logo"
+                  className="w-20 h-20 mx-auto md:mx-0 mb-1"
+                />
               </div>
-              <div className="relative top-0 left-1">
-                <p className="font-playfair text-[19px] font-medium tracking-wide">
-                  FR. CONCEICAO RODRIGUES
-                </p>
-              </div>
+              <div className="pl-0 pt-0">
+                <div className="relative top-3 left-1">
+                  <p className="font-inter text-[10px] font-light leading-9 tracking-[1.0px] italic">
+                    Agnel Charities
+                  </p>
+                </div>
+                <div className="relative top-0 left-1">
+                  <p className="font-playfair text-[19px] font-medium tracking-wide">
+                    FR. CONCEICAO RODRIGUES
+                  </p>
+                </div>
 
-              {/* Subtitle */}
-              <div className="relative top-0 left-1">
-                <p className="font-playfair font-thin text-[13.7px] tracking-[3.7px]">
-                  INSTITUTE OF TECHNOLOGY
-                </p>
-              </div>
+                {/* Subtitle */}
+                <div className="relative top-0 left-1">
+                  <p className="font-playfair font-thin text-[13.7px] tracking-[3.7px]">
+                    INSTITUTE OF TECHNOLOGY
+                  </p>
+                </div>
 
-              {/* VASHI Section with Borders */}
-              <div className="flex items-center justify-center mt-1 relative top-0 -left-3">
-                <span className="border-t border-[#999999] w-[7.65vw]"></span>
-                <p className="font-playfair text-[7.5px] tracking-[2.5px] mx-4 leading-5">
-                  VASHI
-                </p>
-                <span className="border-t border-[#999999] w-[7.65vw]"></span>
+                {/* VASHI Section with Borders */}
+                <div className="flex items-center justify-center mt-1 relative top-0 -left-3">
+                  <span className="border-t border-[#999999] w-[7.65vw]"></span>
+                  <p className="font-playfair text-[7.5px] tracking-[2.5px] mx-4 leading-5">
+                    VASHI
+                  </p>
+                  <span className="border-t border-[#999999] w-[7.65vw]"></span>
+                </div>
               </div>
             </div>
-          </div>
+          </a>
 
           {/* Navigation */}
           <div className="footer-section md:pl-14">
@@ -58,12 +60,24 @@ const Footer = () => {
               Navigation
             </h4>
             <ul className="space-y-2 text-gray-800 font-inter">
-              <li>Home</li>
-              <li>About Us</li>
-              <li>Departments</li>
-              <li>Academics</li>
-              <li>Admission</li>
-              <li>Students</li>
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/aboutUs">About Us</a>
+              </li>
+              <li>
+                <a href="/departments">Departments</a>
+              </li>
+              <li>
+                <a href="/academics">Academics</a>
+              </li>
+              <li>
+                <a href="/admissions">Admissions</a>
+              </li>
+              <li>
+                <a href="/studentCorner">Students Corner</a>
+              </li>
             </ul>
           </div>
 
@@ -73,11 +87,31 @@ const Footer = () => {
               Quick Links
             </h4>
             <ul className="space-y-2 text-gray-800 font-inter">
-              <li>Downloads</li>
-              <li>Feedback</li>
-              <li>Important Links</li>
-              <li>Circulars</li>
-              <li>Fee Payment</li>
+              <li>
+                <a href="/downloads">Downloads</a>
+              </li>
+              <li>
+                <a
+                  href="https://fcrit.ac.in/static_pdfs/feedback/SSS_23_24.pdf"
+                  target="block"
+                >
+                  Feedback
+                </a>
+              </li>
+              <li>
+                <a href="/Important-Links">Important Links</a>
+              </li>
+              <li>
+                <a href="/circulars">Circulars</a>
+              </li>
+              <li>
+                <a
+                  href="https://www.eduqfix.com/PayDirect/#/student/pay/XxoRsO6mfiXGAnQY1R64lGgQcWtw4tYKPWYLPO8nzoGBvtL6DIe+F9YjQMK1keFr/254"
+                  target="block"
+                >
+                  Fee Payment
+                </a>
+              </li>
             </ul>
           </div>
 
