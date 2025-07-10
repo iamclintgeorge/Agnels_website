5bf053457cfaea3e5c6d759de1f9e6e7b624dce6
Clint George <clintbgeorge@gmail.com>
2025-07-10 22:46:18 +0530

    Added roles to DB
    Signed-off-by: Clint George <clintbgeorge@gmail.com>

--- admin/backend/controllers/admin/userController.js
+++ admin/backend/controllers/admin/userController.js
@@ -1,6 +1,5 @@
 import bcrypt from "bcrypt";
 import { signupUser, loginUser } from "../../models/admin/userModels.js";
-import { roleBasedAccess } from "../../utils/permissionMap.js";
 
 export const signupController = async (req, res) => {
   try {
@@ -25,15 +24,12 @@ export const signupController = async (req, res) => {
       return res.status(500).json({ message: "Failed to create user" });
     }
 
-    // Get permissions based on role
-    const permissions = roleBasedAccess[user.role] || [];
-
     req.session.user = {
       id: user.id,
       emailId: user.emailId,
       userName: user.userName,
       role: user.role,
-      permissions: permissions,
+      permissions: user.permissions,
     };
 
     console.log("req.session.user", req.session.user);
@@ -60,15 +56,12 @@ export const loginController = async (req, res) => {
       return res.status(401).json({ message: "Invalid credentials" });
     }
 
-    // Get permissions based on role
-    const permissions = roleBasedAccess[user.role] || [];
-
     req.session.user = {
       id: user.id,
       emailId: user.emailId,
       userName: user.userName,
       role: user.role,
-      permissions: permissions,
+      permissions: user.permissions,
     };
 
     res.status(200).json({
@@ -78,7 +71,7 @@ export const loginController = async (req, res) => {
         emailId: user.emailId,
         userName: user.userName,
         role: user.role,
-        permissions: permissions,
+        permissions: user.permissions,
       },
     });
   } catch (err) {
