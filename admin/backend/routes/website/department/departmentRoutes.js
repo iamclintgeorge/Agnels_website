2e3c8de06da2ba5af921a24ecd52d9c50b21a590
Clint George <clintbgeorge@gmail.com>
2025-08-01 23:51:11 +0530

    Added Vision-Mission and Objectives to Department Home page
    Signed-off-by: Clint George <clintbgeorge@gmail.com>

--- admin/backend/routes/website/department/departmentRoutes.js
+++ admin/backend/routes/website/department/departmentRoutes.js
@@ -1,7 +1,10 @@
 import express from "express";
 import DepartmentController from "../../../controllers/website/departmentController.js";
 import { upload } from "../../../models/website/departmentModel.js";
-import { authMiddleware, checkPermission, } from "../../../middlewares/authMiddleware.js";
+import {
+  authMiddleware,
+  checkPermission,
+} from "../../../middlewares/authMiddleware.js";
 // import { checkPermission } from '../../../utils/permissions.js';
 
 const router = express.Router();
@@ -21,6 +24,49 @@ router.delete(
   authMiddleware,
   checkPermission("department_management"),
   DepartmentController.deleteDeptText
+);
+
+// Dept_Vision and Mission Routes
+router.put(
+  "/vision/:id",
+  authMiddleware,
+  checkPermission("department_management"),
+  DepartmentController.updateDeptVisionText
+);
+
+router.put(
+  "/mission/:id",
+  authMiddleware,
+  checkPermission("department_management"),
+  DepartmentController.updateDeptMissionText
+);
+
+router.put(
+  "/objectives/:id",
+  authMiddleware,
+  checkPermission("department_management"),
+  DepartmentController.updateDeptObjectivesText
+);
+
+router.put(
+  "/outcomes/:id",
+  authMiddleware,
+  checkPermission("department_management"),
+  DepartmentController.updateDeptOutcomesText
+);
+
+router.get("/vision/:departmentId", DepartmentController.getDeptVisionText);
+
+router.get("/mission/:departmentId", DepartmentController.getDeptVisionText);
+
+router.get(
+  "/objectives/:departmentId",
+  DepartmentController.getDeptObjectiveText
+);
+
+router.get(
+  "/outcomes/:departmentId",
+  DepartmentController.getDeptObjectiveText
 );
 
 // DEPT_COMMITTEES ROUTES
